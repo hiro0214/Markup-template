b5caf9ef22dbb88f0ee3f0e500468b4f9a6dc0d2
hiro0214 <leonhurt2@gmail.com>
2022-04-20 23:18:57 +0900

    add imagemin

--- build.ts
+++ build.ts
@@ -10,6 +10,7 @@ const scripts = {
   sass: `sass ${inputDir}/scss/style.scss:${outputDir}/css/style.css -s compressed --no-source-map`,
   postcss: `postcss ${outputDir}/css/style.css -o ${outputDir}/css/style.css`,
   ts: 'node --loader ts-node/esm esbuild.ts',
+  img: 'node imagemin.js',
   tsc: `tsc ${inputDir}/ts/*.ts --noEmit`,
   eslint: `eslint '${inputDir}/ts/*.ts' --fix`,
   stylelint: `stylelint '${inputDir}/scss/**/*.{css,scss,sass}' --fix`,
@@ -34,16 +35,18 @@ const build = async () => {
     const pug = async () => await scriptExec(`${scripts.pug}`);
     const scss = async () => await scriptExec(`${scripts.sass} && ${scripts.postcss}`);
     const ts = async () => await scriptExec(`NODE_ENV=build ${scripts.ts}`);
+    const img = async () => await scriptExec(`${scripts.img}`);
 
-    await lint().then(() => Promise.all([pug(), scss(), ts()]));
+    await lint().then(() => Promise.all([pug(), scss(), ts(), img()]));
   } else {
     console.log(`\n===== Oepn Web Server. Access to \x1b[36mhttp://localhost${port}/ \x1b[0m=====\n`);
     const pug = async () => await scriptExec(`${scripts.pug} -w`);
     const scss = async () => await scriptExec(`${scripts.sass} -w`);
     const ts = async () => await scriptExec(`NODE_ENV=start ${scripts.ts}`);
+    const img = async () => await scriptExec(`watch '${scripts.img}' src/images/`);
     const server = cp.exec(`${scripts.server}`);
 
-    return Promise.all([pug(), scss(), ts(), server]);
+    return Promise.all([pug(), scss(), ts(), img(), server]);
   }
 };
 
